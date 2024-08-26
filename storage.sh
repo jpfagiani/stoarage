0e90cf50c6abfbcf0a78647665af5c7b21fe82eb
jpfagiani <61874180+jpfagiani@users.noreply.github.com>
2024-08-26 14:18:42 -0300

    Update storage.sh

--- storage.sh
+++ storage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-hostnamectl set-hostname storage.cdpni.gov
+hostnamectl set-hostname storage.cdpni.sap
 apt install openssh-server xfsprogs -y
-apt install nfs-common nfs-kernel-server -y
-	systemctl start nfs-server
-	systemctl enable nfs-server                                                                                                                                                                                               	
-	cp -rv /root/storage/etc/* /etc/
+#apt install nfs-common nfs-kernel-server -y
+#	systemctl start nfs-server
+#	systemctl enable nfs-server                                                                                                                                                                                               	
+	cp -v /root/storage/etc/* /etc/
 	chattr +i /etc/resolv.conf
 	systemctl restart networking sshd
 	#mkfs -t xfs /dev/sdb1
@@ -16,10 +16,10 @@ apt install nfs-common nfs-kernel-server -y
 		tune2fs -L disciplina /dev/sdc1
 	mkfs -t ext4 /dev/sdd1
 		tune2fs -L publico /dev/sdd1
-	mkdir -pv /srv/eat /srv/disciplina /srv/pulbico
+	mkdir -pv /srv/eat /srv/disciplina /srv/publico
 		echo LABEL=eat /srv/eat ext4 defaults 0 0 >> /etc/fstab
 		echo LABEL=disciplina /srv/disciplina ext4 defaults 0 0 >> /etc/fstab
-		echo LABEL=publico    /srv/pulbico    ext4 defaults 0 0 >> /etc/fstab
+		echo LABEL=publico    /srv/publico    ext4 defaults 0 0 >> /etc/fstab
 for i in .cpd .homes .drivers .lixeiras publico; do mkdir -pv /srv/publico/$i; done 
 for i in sindicancia supervisao educacao cimic; do mkdir -pv /srv/eat/$i; done
 for i in chefia_turno_I chefia_turno_II chefia_turno_III chefia_turno_IV conexao_familiar administrativo aevp almoxarifado canil dcsd financas inclusao infraestrutura saude; do mkdir -pv /srv/disciplina/$i; done
